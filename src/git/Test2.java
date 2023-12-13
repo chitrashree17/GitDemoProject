3ec45131af10d318eea29a79ebcfc2ab134b2574
Chitra <Chitra@DESKTOP-F9MGNGH>
2023-12-13 16:55:23 +0530

    First commit

---
+++ src/git/Test2.java
@@ -0,0 +1,17 @@
+package git;
+
+public class Test2 {
+	
+	public static void main(String[] args)
+	{
+		String hungry="yes";
+		
+		if (hungry=="yes")
+			System.out.println("go eat something.....");
+		else {
+			System.out.println("Do work.....");
+		}
+			
+	}
+
+} 	
